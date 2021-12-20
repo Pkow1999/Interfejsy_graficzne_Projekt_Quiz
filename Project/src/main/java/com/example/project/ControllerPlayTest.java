1c7a2cf4af14c8d174ffd9cd0a87cf44aca2cd3b
Patryk Wypych <patryk.wypych7@gmail.com>
2021-12-20 21:06:52 +0100

    przycik cofania do menu z rozwiązywania quizu

--- Project/src/main/java/com/example/project/ControllerPlayTest.java
+++ Project/src/main/java/com/example/project/ControllerPlayTest.java
@@ -1,11 +1,24 @@
 package com.example.project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class ControllerPlayTest {
     @FXML
     private Label questionText;
+
+    private Parent home_page_parent;
+    private Scene home_page_scene;
+    private Stage stage;
 
     @FXML
     protected void onHello1ButtonClick() {questionText.setText("1");}
@@ -15,4 +28,14 @@ public class ControllerPlayTest {
     protected void onHello3ButtonClick() {questionText.setText("3");}
     @FXML
     protected void onHello4ButtonClick() {questionText.setText("4");}
+    @FXML
+    protected void onExitButtonClick(ActionEvent event) throws IOException {
+        home_page_parent= FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
+        //to sie rozpierdala - bo nakladamy kolejna wartswe zamiast wrocic do poprzedniej
+        //TODO
+        home_page_scene =  new Scene(home_page_parent);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(home_page_scene);
+        stage.show();
+    }
 }
